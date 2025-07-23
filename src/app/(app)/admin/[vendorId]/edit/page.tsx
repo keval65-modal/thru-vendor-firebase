f74ff7d090e07cfe1685a3e01ad4f416fe5886a7
Keval Tanna <Keval65@gmail.com>
2025-07-23 11:17:29 +0000

    starting build "2df91f54-5cf8-4e76-9498-4067ce3c64e5" FETCHSOURCE hint:

--- src/app/(app)/admin/[vendorId]/edit/page.tsx
+++ src/app/(app)/admin/[vendorId]/edit/page.tsx
@@ -7,13 +7,7 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { ArrowLeft } from 'lucide-react';
 
-type EditVendorPageProps = {
-  params: {
-    vendorId: string;
-  };
-};
-
-export default async function EditVendorPage({ params }: EditVendorPageProps) {
+export default async function EditVendorPage({ params }: { params: { vendorId: string } }) {
   const { vendor, error } = await getVendorForEditing(params.vendorId);
 
   if (error) {
