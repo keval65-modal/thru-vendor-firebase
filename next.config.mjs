e50a12e61cc54c8950725280ad027c392e4c74c6
Keval Tanna <Keval65@gmail.com>
2025-07-24 07:38:10 +0000

    Nope

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // The env block is added here to ensure the environment variables
-  // are correctly exposed to the client-side code during the build process.
+  reactStrictMode: true,
   env: {
     NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
