a5b976dc95848a3f761dee6c1ea999e0b65a3e30
Keval Tanna <Keval65@gmail.com>
2025-07-25 09:54:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 
-module.exports = {
+const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
+
+export default config;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
 
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
-const config = {
+const config: Config = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
@@ -19,7 +19,7 @@ const config = {
       },
     },
     extend: {
-      fontFamily: {
+       fontFamily: {
         sans: ["var(--font-sans)", "DM Sans", "sans-serif"],
       },
       colors: {
@@ -62,23 +62,9 @@ const config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [],
+};
 
-export default config
+export default config;
