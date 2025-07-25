9567d9bfb299889b968f6355c9632fb446c8015e
Keval Tanna <Keval65@gmail.com>
2025-07-25 10:29:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -4,11 +4,11 @@ import { getApps, getApp, initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
 import { getStorage } from 'firebase-admin/storage';
-import serviceAccount from '../../../serviceAccountKey.json';
+import serviceAccount from '../../serviceAccountKey.json';
 
 const app = !getApps().length
   ? initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
       storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     })
   : getApp();
