1d7a1449a6374ad6d26c059ce676d53056798917
Keval Tanna <Keval65@gmail.com>
2025-07-25 07:55:54 +0000

    Why is all the UI gone from all pages?

--- workspace/next.config.js
+++ workspace/next.config.js
@@ -1,19 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'storage.googleapis.com',
-      }
+      },
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
