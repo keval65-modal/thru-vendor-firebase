5bdfd18712f8155ff8ef7903a283197756dd7ea8
Keval Tanna <Keval65@gmail.com>
2025-07-25 13:47:27 +0000

    starting build "b61f9d7b-0ad5-4fbd-9cf7-5bd2ffd7f8cd" FETCHSOURCE hint:

---
+++ workspace/src/lib/firebase-admin.ts
@@ -0,0 +1,21 @@
+
+import admin from 'firebase-admin';
+import { getApps, getApp, initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import { getAuth } from 'firebase-admin/auth';
+import { getStorage } from 'firebase-admin/storage';
+import serviceAccount from '/workspace/serviceAccountKey.json';
+
+const app = !getApps().length
+  ? initializeApp({
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`
+    })
+  : getApp();
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { db, auth, storage };
